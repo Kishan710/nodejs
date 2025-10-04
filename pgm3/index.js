2e1378fdc34ecff44ee8416dd011051b12989c6a
Kishan710 <7prajapati1kishan0@gmail.com>
2025-10-04 13:53:19 +0530

    Add files via upload

---
+++ pgm3/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const { router } = require("./routes/route")
+const app = express()
+
+const PORT = 8080
+
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/", router)
+
+app.listen(PORT, (err) => {
+    if(err){
+        console.log("server is not running", PORT)
+        return
+    }
+    console.log("server is running", PORT)
+})
