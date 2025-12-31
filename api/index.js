5b8ded8b25804eb2b407a72c3b52ca42fcf1f518
Kishan710 <7prajapati1kishan0@gmail.com>
2025-12-31 14:22:54 +0530

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const { connectDB } = require("./config/db")
+const { userRouter } = require("./routes/userRoutes")
+const app = express()
+require('dotenv').config() 
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use("/add", userRouter)
+
+app.get("/", (req, res) => {
+  return res.status(300).json({
+    message: "hello world"
+  })
+})
+
+app.listen(process.env.PORT, (err) => {
+    if (err) {
+        console.log("server isn't connected")
+    }
+    connectDB()
+    console.log("server is connected", process.env.PORT)
+})
